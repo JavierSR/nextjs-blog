644a85ff7e85b3f857171fc7694b4bf12b89a089
Oscar Sandoval <javier45x@gmail.com>
2022-03-19 23:17:54 -0500

    Post media queries

--- components/layout.tsx
+++ components/layout.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const Layout = ({ children } : { children: React.ReactNode }) => (
     <>
         <Head>
-            <title>Oscar Sandoval</title>
+            <title>Píxeles sin contexto</title>
             <meta name='description' content='Mis opiniones y pensamientos' />
             <link rel='icon' href='/favicon.ico' />
         </Head>
