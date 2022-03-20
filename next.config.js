d78251a71f015484d6d844e370623919b66bfcb6
Oscar Sandoval <javier45x@gmail.com>
2022-03-19 23:53:12 -0500

    Next config removed locales

--- next.config.js
+++ next.config.js
@@ -1,10 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  i18n: {
-    locales: ['en-US', 'es'],
-    defaultLocale: 'es',
-  },
   images: {
     domains: ['images.ctfassets.net']
   }
